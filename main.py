b892b5a1eb494bccb2092f9160a9110817f95d75
Sadie Swen <110064474+s-swen@users.noreply.github.com>
2025-04-18 06:43:23 +0000

    annotated review started

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ BANDS = [
 async def bands(
     genre: GenreURLChoices | None = None,
     has_albums: bool = False
-) -> list[Band]:
-    band_list = [Band(**band) for band in BANDS]
+) -> list[BandWithID]:
+    band_list = [BandWithID(**band) for band in BANDS]
     if genre:
         return [
-            band for band in band_list if band.genre.lower() == genre.value
+            band for band in band_list if band.genre.value.lower() == genre.value
         ]
     if has_albums:
         return [
@@ -31,14 +31,15 @@ async def bands(
     return band_list
 
 @app.get('/bands/{band_id}')
-async def band(band_id: int) -> Band:
-    band = next((Band(**band) for band in BANDS if band['id'] == band_id), None)
+async def band(band_id: int) -> BandWithID:
+    band = next((BandWithID(**band) for band in BANDS if band['id'] == band_id), None)
     if not band:
         raise HTTPException(status_code=404, detail="band not found")
     return band
 
-# @app.get('/bands/genre/{genre}')
-# async def genre(genre: GenreURLChoices) -> list[Band]:
-#     return [
-#         Band(**band) for band in BANDS if band['genre'].lower() == genre.value
-#     ]
+@app.post('/bands')
+async def create_band(band_data: BandCreate) -> BandWithID:
+    id = BANDS[-1]['id'] + 1
+    band = BandWithID(id=id, **band_data.model_dump()).model_dump()
+    BANDS.append(band)
+    return band
--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from datetime import date
 
 
@@ -9,17 +9,25 @@ class GenreURLChoices(Enum):
     METAL = 'metal'
     HIP_HOP = 'hip-hop'
 
+class GenreChoices(Enum):
+    ROCK = 'Rock'
+    ELECTRONIC = 'Electronic'
+    METAL = 'Metal'
+    HIP_HOP = 'Hip-Hop'
+
 class Album(BaseModel):
     title: str
     release_date: date
 
 class BandBase(BaseModel):
     name: str
-    genre: str
+    genre: GenreChoices
     albums: list[Album] = []
 
 class BandCreate(BandBase):
-    pass
+    @field_validator('genre', mode='before')
+    def title_case_genre(cls, value):
+        return value.title()
 
 class BandWithID(BandBase):
     id: int
